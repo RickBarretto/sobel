fcfd2f9c1381e977137a7458f4f174acaec3d1f5
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-06-03 20:13:03 -0300

    create CLI interface

---
+++ include/app/cli.h
@@ -0,0 +1,41 @@
+#ifndef APP_CLI_H
+#define APP_CLI_H
+
+#include <app/types.h>
+
+/// bmp_t << cstring_t : Bitmap's Path
+typedef cstring_t bmp_t;
+
+
+/// Command Line Interface representation.
+///
+/// Application's CLI has two positional arguments: `<input>` and `<output>`.
+/// Each one is a Path to a BitMap image.
+typedef struct {
+    cstring_t app;
+    cstring_t usage;
+
+    // Positional Arguments
+    bmp_t input_file;
+    bmp_t output_file;
+} Cli;
+
+
+/// Create a CLI object from command-line.
+///
+/// Params
+/// ------
+/// app: cstring_t
+///    Application name to be displayed. 
+/// arguments: cstring_t[] 
+///     Values from CLI. example: ["input.bpm", "output.bmp"]
+/// count: size_t
+///     Arguments count. exammple: 2
+Cli * cli_from(cstring_t app, cstring_t arguments[], size_t count);
+
+
+/// Free CLI and its fields from memory.
+void cli_free(Cli* cli);
+
+
+#endif
---
+++ include/app/types.h
@@ -0,0 +1,10 @@
+#ifndef FRONTIER_TYPES_H
+#define FRONTIER_TYPES_H
+
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef char* cstring_t;
+typedef int exit_t;
+
+#endif
