90d25489a238950f6d7a6b360e570a6cd092fdbe
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-06-03 21:23:50 -0300

    add conditional flag for production

--- src/app.c
+++ src/app.c
@@ -6,6 +6,12 @@
 #define APP_NAME "sobel"
 #define APP_DESCRIPTION "Apply border filters to bitmap images."
 
+#ifdef IN_PROD
+    #define USE_LAPLACE_MPU_V2 true
+#else
+    #define USE_LAPLACE_MPU_V2 false
+#endif
+
 exit_t main(
     size_t arguments_length, 
     cstring_t arguments[]
@@ -20,6 +26,8 @@ exit_t main(
 
     printf("Got:\t%s\n", cli->input_file);
     printf("To:\t%s\n", cli->output_file);
+
+    printf("USE_LAPLACE_MPU_V2=%d\n", USE_LAPLACE_MPU_V2);
 
     cli_free(cli);
 
